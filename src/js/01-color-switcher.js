ff2675a66de4ce5d2adfc0d33c5f22d40a8b03b3
Marcin Wrembel <marcin.wrembel@interia.pl>
2022-11-25 22:03:35 +0100

    1st task done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let colorChange = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', () => {
+  colorChange = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.setAttribute('disabled', '');
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(colorChange);
+  btnStart.removeAttribute('disabled');
+});
+
+/*
+W HTML znajdują się przyciski «Start» i «Stop».
+
+<button type="button" data-start>Start</button>
+<button type="button" data-stop>Stop</button>
+
+Napisz skrypt, który po kliknięciu przycisku «Start», raz na sekundę zmienia kolor tła <body> na wartość losową używając stylu inline. Po kliknięciu przycisku «Stop», kolor tła powinien przestać się zmieniać.
+  */
